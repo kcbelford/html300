101998f88475b159163c6f7375ca62b72edce8f1
kcbelford <kcbelford@hotmail.com>
2020-04-22 13:18:00 -0700

    completed lesson03 activity

--- lesson03/activity/index.js
+++ lesson03/activity/index.js
@@ -1,29 +1,58 @@
-// Create the Dog class constructor
-class Dog {
-	// Give it expected parameters
-	constructor(name, age, breed, color) {
-		// Set those equal to the instance
-		this.name = name;
-		this.age = age;
-		this.breed = breed;
-		this.color = color;
-		this.energyLevel = 0;
-		this.barkLevel = 10;
+// // Create the Dog class constructor
+// class Dog {
+// 	// Give it expected parameters
+// 	constructor(name, age, breed, color) {
+// 		// Set those equal to the instance
+// 		this.name = name;
+// 		this.age = age;
+// 		this.breed = breed;
+// 		this.color = color;
+// 		this.energyLevel = 0;
+// 		this.barkLevel = 10;
+// 	}
+// 	// pat method
+// 	pat() {
+// 		this.energyLevel++
+// 		this.barkLevel--;
+// 		console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and current bark level is ${this.barkLevel}.`);
+// 	}
+// }
+//
+// // Create marty
+// const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
+//
+// // Pat him 5 times
+// marty.pat();
+// marty.pat();
+// marty.pat();
+// marty.pat();
+// marty.pat();
+
+class Character {
+	constructor(name, species, weapon, affiliation) {
+	this.name = name;
+	this.species = species;
+	this.weapon = weapon;
+	this.affiliation = affiliation;
+	this.energyLevel = 10;
 	}
-	// pat method
-	pat() {
-		this.energyLevel++
-		this.barkLevel--;
-		console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and current bark level is ${this.barkLevel}.`);
+
+	attack() {
+		this.energyLevel--;
+		console.log(`This charachter's name is ${this.name}, they are a ${this.species}, their weapon of choice is ${this.weapon}, their affiliation is ${this.affiliation}, their energy level is ${this.energyLevel}`);
+	}
+
+	heal() {
+		this.energyLevel++;
+		console.log(`This charachter's name is ${this.name}, they are a ${this.species}, their weapon of choice is ${this.weapon}, their affiliation is ${this.affiliation}, their energy level is ${this.energyLevel}`);
 	}
 }
 
-// Create marty
-const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
+const mario = new Character('Mario', 'human', 'fireball', 'good');
 
-// Pat him 5 times
-marty.pat();
-marty.pat();
-marty.pat();
-marty.pat();
-marty.pat();
+mario.attack();
+mario.attack();
+mario.attack();
+mario.attack();
+mario.heal();
+mario.heal();
